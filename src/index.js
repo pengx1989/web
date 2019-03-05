f2183d0d5b3de237b76c9db04d40257dbef59eaf
jackxu115 <pengxu19890115@gmail.com>
2019-03-05 02:47:52 -0800

    index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'
+//import App from './App';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var data = 123;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class MyTitle extends React.Component {
+    static propTypes = {
+        title: PropTypes.number.isRequired,
+    }
+    render() {
+        return <h1> {this.props.title} </h1>;
+    }
+}
+
+
+ReactDOM.render(<MyTitle title={data} />, document.getElementById('root'));
